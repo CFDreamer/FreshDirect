287f5794458179179711fe5e44fc4b7a4a558d92
CFDreamer <39845479+CFDreamer@users.noreply.github.com>
2019-04-05 18:38:03 +0800

    1

--- Code/FreshDirect/src/test/java/com/yearjane/dao/GoodsDaoTest.java
+++ Code/FreshDirect/src/test/java/com/yearjane/dao/GoodsDaoTest.java
@@ -11,77 +11,77 @@ import com.yearjane.basetest.BaseTest;
 import com.yearjane.entity.GoodsType;
 
 public class GoodsDaoTest extends BaseTest{
-	@Autowired
-    private GoodsDao dao;
-	
-	@Test
-	public void test() {
-		GoodsType goodsType=new GoodsType();
-		GoodsType parent=new GoodsType();
-		parent.setTypeid(1);
-		goodsType.setParent(parent);
-		List<GoodsType> list=dao.queryGoodsTypeList(goodsType);
-		System.err.println(list);
-	}
-	@Test
-	public void testUpdate() {
-	    GoodsType goodsType=new GoodsType(); 
-	    goodsType.setTypeid(9);
-//	    goodsType.setTypename("新鲜的蔬菜");
-//	    goodsType.setOperatorName("chanfrer3");
-//	    goodsType.setUpdateTime(new Date());
-	    goodsType.setIsdeleted(0);
-	    System.out.println(dao.updateGoodsType(goodsType));
-	    
-	}
-	@Test
-	public void testAdd() {
-		List<GoodsType> list=new ArrayList<GoodsType>() ;
-		GoodsType parent=new GoodsType();
-		parent.setTypeid(1);
-		GoodsType g=null;
-		g=new GoodsType();
-		g.setTypename("测试新增商品1");
-		g.setOperatorName("chanfrer2");
-		g.setParent(parent);
-		g.setAddTime(new Date());
-		g.setUpdateTime(new Date());
-		list.add(g);
-		
-		parent=new GoodsType();
-		parent.setTypeid(2);
-		
-		g=new GoodsType();
-		g.setTypename("测试新增商品2");
-		g.setOperatorName("chanfrer2");
-		g.setParent(parent);
-		g.setAddTime(new Date());
-		g.setUpdateTime(new Date());
-		list.add(g);
-		
-		g=new GoodsType();
-		g.setTypename("测试新增商品3");
-		g.setOperatorName("chanfrer2");
-		g.setAddTime(new Date());
-		g.setUpdateTime(new Date());
-		list.add(g);
-		
-		g=new GoodsType();
-		g.setTypename("测试新增商品4");
-		g.setOperatorName("chanfrer2");
-		g.setAddTime(new Date());
-		g.setUpdateTime(new Date());
-		list.add(g);
-		
-		g=new GoodsType();
-		g.setTypename("测试新增商品5");
-		g.setOperatorName("chanfrer2");
-		g.setAddTime(new Date());
-		g.setUpdateTime(new Date());
-		list.add(g);
-		
-		dao.addGoodsType(list);
-		
-	}
+//	@Autowired
+//    private GoodsDao dao;
+//	
+//	@Test
+//	public void test() {
+//		GoodsType goodsType=new GoodsType();
+//		GoodsType parent=new GoodsType();
+//		parent.setTypeid(1);
+//		goodsType.setParent(parent);
+//		List<GoodsType> list=dao.queryGoodsTypeList(goodsType);
+//		System.err.println(list);
+//	}
+//	@Test
+//	public void testUpdate() {
+//	    GoodsType goodsType=new GoodsType(); 
+//	    goodsType.setTypeid(9);
+////	    goodsType.setTypename("新鲜的蔬菜");
+////	    goodsType.setOperatorName("chanfrer3");
+////	    goodsType.setUpdateTime(new Date());
+//	    goodsType.setIsdeleted(0);
+//	    System.out.println(dao.updateGoodsType(goodsType));
+//	    
+//	}
+//	@Test
+//	public void testAdd() {
+//		List<GoodsType> list=new ArrayList<GoodsType>() ;
+//		GoodsType parent=new GoodsType();
+//		parent.setTypeid(1);
+//		GoodsType g=null;
+//		g=new GoodsType();
+//		g.setTypename("测试新增商品1");
+//		g.setOperatorName("chanfrer2");
+//		g.setParent(parent);
+//		g.setAddTime(new Date());
+//		g.setUpdateTime(new Date());
+//		list.add(g);
+//		
+//		parent=new GoodsType();
+//		parent.setTypeid(2);
+//		
+//		g=new GoodsType();
+//		g.setTypename("测试新增商品2");
+//		g.setOperatorName("chanfrer2");
+//		g.setParent(parent);
+//		g.setAddTime(new Date());
+//		g.setUpdateTime(new Date());
+//		list.add(g);
+//		
+//		g=new GoodsType();
+//		g.setTypename("测试新增商品3");
+//		g.setOperatorName("chanfrer2");
+//		g.setAddTime(new Date());
+//		g.setUpdateTime(new Date());
+//		list.add(g);
+//		
+//		g=new GoodsType();
+//		g.setTypename("测试新增商品4");
+//		g.setOperatorName("chanfrer2");
+//		g.setAddTime(new Date());
+//		g.setUpdateTime(new Date());
+//		list.add(g);
+//		
+//		g=new GoodsType();
+//		g.setTypename("测试新增商品5");
+//		g.setOperatorName("chanfrer2");
+//		g.setAddTime(new Date());
+//		g.setUpdateTime(new Date());
+//		list.add(g);
+//		
+//		dao.addGoodsType(list);
+//		
+//	}
  
 }
--- Code/FreshDirect/src/test/java/com/yearjane/dao/UserInfoDaoTest.java
+++ Code/FreshDirect/src/test/java/com/yearjane/dao/UserInfoDaoTest.java
@@ -10,22 +10,22 @@ import com.yearjane.basetest.BaseTest;
 import com.yearjane.entity.UserInfo;
 
 public class UserInfoDaoTest extends BaseTest {
-	@Autowired
-	private UserInfoDao dao;
-	@Test
-	public void getUserInfoList() {
-		UserInfo userInfo=new UserInfo();
-		userInfo.setUsername("chanfrer");
-		List<UserInfo> list=dao.getUserInfoList(userInfo);
-		System.out.println(list);
-	}
-	@Test
-	public void updateUserInfoTest() {
-		UserInfo userInfo=new UserInfo();
-		userInfo.setUsername("chanfrer");;
-		userInfo.setRemarks("修改了头像");
-		userInfo.setUpdateTime(new Date());
-		userInfo.setImagePath("/test/test/test");
-		System.out.println(dao.udpateUserInfo(userInfo));
-	}
+//	@Autowired
+//	private UserInfoDao dao;
+//	@Test
+//	public void getUserInfoList() {
+//		UserInfo userInfo=new UserInfo();
+//		userInfo.setUsername("chanfrer");
+//		List<UserInfo> list=dao.getUserInfoList(userInfo);
+//		System.out.println(list);
+//	}
+//	@Test
+//	public void updateUserInfoTest() {
+//		UserInfo userInfo=new UserInfo();
+//		userInfo.setUsername("chanfrer");;
+//		userInfo.setRemarks("修改了头像");
+//		userInfo.setUpdateTime(new Date());
+//		userInfo.setImagePath("/test/test/test");
+//		System.out.println(dao.udpateUserInfo(userInfo));
+//	}
 }
